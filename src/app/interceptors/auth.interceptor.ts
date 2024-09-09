7b569f98805fe83665f6bb980e8e50936ce055a0
henrylacava <lacavahenry@gmail.com>
2024-09-09 20:09:35 -0300

    commit

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -3,7 +3,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('auth-token');
 
-  if (token) {
+  if (token && req.url !== 'http://localhost:8080/api/auth/login') {
     req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
